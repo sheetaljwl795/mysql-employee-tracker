c44485849e76b06baa92d87f8a9c45236faca42f
sheetaljwl795 <sheetaljwl795@gmail.com>
2022-09-23 09:00:38 -0700

    Added FOREIGN KEY (manager_id), added some Sql queries

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,23 +1,29 @@
 INSERT INTO department (id, name)
-VALUES ("Web Development"),
-       ("Data Science"),
-       ("Math"),
-       ("Electives");
+VALUES (1, "Sales"),
+       (2, "Engineering"),
+       (3, "Finance"),
+       (4, "Legal");
 
-INSERT INTO role (id, title, salary, department_id)
-VALUES ("Intro to JavaScript", 1),
-       ("Data Science", 2),
-       ("Linear Algebra", 3),
-       ("History of the Internet", 4),
-       ("Machine Learning", 4),
-       ("Game Design", 1 ),
-       ("Cloud Development", 1);
+INSERT INTO role (id,  title, salary, department_id)
+VALUES (1, "Sales Lead", 100000, 1),
+       (2, "Salesperson", 80000, 1),
+       (3, "Lead Engineer", 150000, 2),
+       (4, "Software Engineer", 120000, 2),
+       (5, "Accountant", 125000, 3),
+       (6, "Account Manager",16000,3),
+       (7, "Legal Team Lead", 250000,4),
+       (8, "Lawyer", 19000,4);
+
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES ("Intro to JavaScript", 1),
-       ("Data Science", 2),
-       ("Linear Algebra", 3),
-       ("History of the Internet", 4),
-       ("Machine Learning", 4),
-       ("Game Design", 1 ),
-       ("Cloud Development", 1);
+VALUES (1, "Jane", "Doe", 1, null),
+       (2, "Mike","Chan",1,1),
+       (3, "Ashely", "Rodrigez", 2, 1),
+       (4, "Kevin","Tupik",2, 1),
+       (5, "Mike", "Milchor",3,2),
+       (6, "Even","Jhoan",3,null ),
+       (7,"Anil","kapoor",4,4),
+       (8,"Jaya","Devi",4,null),
+       (9,"Kyle","Groge",1,3),
+       (10, "Gary", "Lia", 2, null);
+
--- server.js
+++ server.js
@@ -1,5 +1,85 @@
-const express = require('express');
 const inquirer = require('inquirer');
 const fs = require('fs');
 const mysql = require('mysql2');
 const cTable = require('console.table');
+const PORT = process.env.PORT || 3006;
+const app = express();
+
+// Connect to the employee database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // MySQL password
+      password: '',
+      database: 'employee_db'
+    },
+    console.log(`Connected to the employee_db database.`)
+    );
+    db.query('SELECT * FROM students', function (err, results) {
+        console.log(results);
+    
+      });
+
+      const initalPrompt = () => {
+        const FirstQuestion = [{
+            type:"list",
+            name:"action",
+            message:" What would you like to do?",
+            choices:["Viwe all employees", "View all roles","View all departments","Add an employee","Add a role","Add a department","Update role for an employee","update employee's manager", "view employees by manager", "delete a department", "delete a role", "delete an employee", "View the total utilized budget of a department", "quit"]
+        }]
+        inquier.prompt(startQuestion)
+  .then(response => {
+    switch (response.action) {
+      case "View all employees":
+        viewAll("EMPLOYEE");
+        break;
+      case "View all roles":
+        viewAll("ROLE");
+        break;
+      case "View all departments":
+        viewAll("DEPARTMENT");
+        break;
+      case "add a department":
+        addNewDepartment();
+        break;
+      case "add a role":
+        addNewRole();
+        break;
+      case "add an employee":
+        addNewEmployee();
+        break;
+      case "update role for an employee":
+        updateRole();
+        break;
+      case "view employees by manager":
+        viewEmployeeByManager();
+        break;
+      case "update employee's manager":
+        updateManager();
+        break;
+      case "delete a department":
+        deleteDepartment();
+        break;
+      case "delete a role":
+        deleteRole();
+        break;
+      case "delete an employee":
+        deleteEmployee();
+        break;
+      case "View the total utilized budget of a department":
+        viewBudget();
+        break;
+      default:
+        connection.end();
+    }
+  })
+  .catch(err => {
+    console.error(err);
+  });
+}
+        
+            
+            
+            
